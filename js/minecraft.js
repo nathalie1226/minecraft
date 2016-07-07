1088f66f3bf90eb61b40b97f6e48d45ca53ac0c5
nathalie <nathalie1226@hotmail.com>
2016-07-07 18:19:39 +0300

    working game

--- js/minecraft.js
+++ js/minecraft.js
@@ -1,5 +1,5 @@
-var minecraft = {};
-
+var minecraft = {}; //creates the minecraft object
+//creating the matrix
 minecraft.matrix = [
     ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
     ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
@@ -23,6 +23,58 @@ minecraft.matrix = [
     ["dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt"]
 
 ];
+
+//creating the answer matrix
+minecraft.AnswerMatrix = [
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "cloud", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "cloud", "cloud", "cloud", "cloud", "", "cloud", "cloud", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "cloud", "cloud", "cloud", "cloud", "cloud", "cloud", "cloud", "cloud", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "cloud", "cloud", "", "", "", "", "", "", "leaf", "leaf", "leaf", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "leaf", "leaf", "leaf", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "leaf", "leaf", "leaf", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "tree", "", "", ""],
+    ["", "", "", "", "leaf", "", "", "", "", "", "", "", "", "", "", "", "tree", "", "", ""],
+    ["", "", "", "leaf", "leaf", "leaf", "", "", "", "", "", "", "", "rock", "rock", "", "tree", "", "", "rock"],
+    ["grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass"],
+    ["dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt"],
+    ["dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt"],
+    ["dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt"],
+    ["dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt"]
+
+
+];
+minecraft.AnswerMatrix2 = [
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "cloud", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "cloud", "cloud", "cloud", "cloud", "", "cloud", "cloud", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "cloud", "cloud", "cloud", "cloud", "cloud", "cloud", "cloud", "cloud", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "cloud", "cloud", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "leaf", "leaf", "leaf", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "leaf", "leaf", "leaf", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "leaf", "leaf", "leaf", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "tree", "", "", ""],
+    ["", "", "", "rock", "", "", "", "", "", "", "", "", "", "", "", "", "tree", "", "", ""],
+    ["", "", "rock", "rock", "rock", "", "", "", "", "", "", "", "", "leaf", "leaf", "", "tree", "", "", "leaf"],
+    ["grass", "grass", "grass", "grass", "grass", "", "", "", "", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass"],
+    ["dirt", "dirt", "dirt", "dirt", "dirt", "", "", "", "", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt"],
+    ["dirt", "dirt", "dirt", "dirt", "dirt", "", "", "", "", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt"],
+    ["dirt", "dirt", "dirt", "dirt", "dirt", "", "", "", "", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt"],
+    ["dirt", "dirt", "dirt", "dirt", "dirt", "", "", "", "", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt"]
+
+
+];
+
+//creating the board
 for (var i = 0; i < 20; i++) {
     var bigBox = $("<div/>");
     bigBox.addClass("row");
@@ -34,56 +86,48 @@ for (var i = 0; i < 20; i++) {
     }
 
 }
+minecraft.selectedImage = function () {
+    if ($(".example.selected").length == 0 ) {
+        var selected = $(this);
+        selected.addClass('selected');
+        if (selected.attr('id') === "first") {
+            minecraft.example = minecraft.AnswerMatrix
+        }
+        else if (selected.attr('id') === "second") {
+            minecraft.example = minecraft.AnswerMatrix2
+        }
+    }
+};
+minecraft.shuffle = function () {
 
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+
+};
+//function initialisation that will be called first
 minecraft.init = function () {
 
-
-    var toolSet = document.createElement('div');
-    toolSet.id = "toolSet";
-    document.body.appendChild(toolSet);
-
-    var toolTree = document.createElement('div');
-    toolTree.id = "axe";
-    toolTree.className = "tool";
-    toolSet.appendChild(toolTree);
-
-    var toolRock = document.createElement('div');
-    toolRock.id = "pick";
-    toolRock.className = "tool";
-    toolSet.appendChild(toolRock);
-
-    var toolGround = document.createElement('div');
-    toolGround.id = "shovel";
-    toolGround.className = "tool";
-    toolSet.appendChild(toolGround);
-
-    var toolGround = document.createElement('div');
-    toolGround.id = "saved";
-    toolGround.className = "storedTile";
-    toolSet.appendChild(toolGround);
-
-    $('#saved').on('click',minecraft.clickedSavedTile);
-    $('.sky').on('contextmenu',function(event){
-        if (minecraft.tool==="") {
-            var savedTile = $('.storedTile').removeClass('storedTile');
-            this.className += savedTile;
-        }
-    });
-    // minecraft.replaceSavedTile=function(clickEvent){
-    //     clickEvent.preventDefault();
-    //     if (minecraft.tool==="") {
-    //         var savedTile = $('.storedTile').removeClass('storedTile');
-    //         this.className += savedTile;
-    //     }
-    //     // this.className==='box sky'
-    // };
-    // $(this).bind("contextmenu", function(e) {
-    //     e.preventDefault();
-    // });
-
+    $('img').on("click", minecraft.selectedImage);
+    $('#treasure').attr("data-saved-tile", "");
     minecraft.tool = "shovel";
     minecraft.tile = "";
     minecraft.storedTile = "";
+    minecraft.example=[];
+    $('#submit').on('click', minecraft.checkIfWon);
 
     minecraft.tools = $('.tool');
     minecraft.tools.on('click', minecraft.clickedTool);
@@ -95,6 +139,7 @@ minecraft.init = function () {
         for (var j = 0; j < minecraft.matrix[i].length; j++) {
             if (minecraft.matrix[i][j] === "") {
                 minecraft.matrix[i][j] = "sky";
+                minecraft.AnswerMatrix[i][j] = "sky";
             }
             $('.box').eq(i * 20 + j).data("i", i).data("j", j).addClass(minecraft.matrix[i][j]);
 
@@ -103,75 +148,82 @@ minecraft.init = function () {
 };
 
 minecraft.clickedTool = function (event) {
-
     minecraft.tool = this.id;
-    console.log(minecraft.tool);
-
 };
 
-minecraft.clickedSavedTile=function(){
- minecraft.tool="";
+
+
+
+minecraft.checkIfWon = function () {
+    var foundProblem = false;
+    for (var i = 0; i < minecraft.matrix.length; i++) {
+        for (var j = 0; j < minecraft.matrix[i].length; j++) {
+            if (minecraft.matrix[i][j] !== minecraft.example[i][j]) {
+                foundProblem = true;
+                break;
+            }
+        }
+    }
+    if (!foundProblem) {
+        $('#message').text('congrats');
+        $('#lightbox').show();
+    } else {
+        $('#message').text('sorry you lost');
+        $('#lightbox').show();
+    }
 };
-
-// minecraft.clickedTile = function (event) {
-//
-//     minecraft.tile = event.target.className;
-//     console.log(minecraft.tile);
-//
-// };
-
+//function that checks if the tool selected can be used on the tile selected 
 minecraft.checkIfBoxMatches = function () {
-    console.log($(this));
 
-    var tileClicked = this;
-    console.log(tileClicked);
-    // console.log($(this).data('i'));
+    var tileClicked = $(this);
+    minecraft.tile = tileClicked.prop('class').replace("box ", "");
 
-    minecraft.tile = tileClicked.className;
-    console.log(minecraft.tile);
-    if (minecraft.tool === "axe" && (minecraft.tile === "box tree" || minecraft.tile === "box leaf")) {
-        minecraft.storedTile = minecraft.tile;
-        $('.storedTile').removeClass().addClass(minecraft.tile).removeClass('box').addClass('storedTile');
-        tileClicked.className="box";
-        minecraft.matrix[$(this).data("i")][($(this)).data("j")] = 'box';
-        minecraft.renderBoard();
-        console.log($(this).data("i"));
-        console.log($(this).data("j"));
-        console.log(minecraft.matrix);
+
+    if (minecraft.tool === "axe" && (tileClicked.hasClass("tree") || tileClicked.hasClass("leaf"))) {
+        minecraft.moveTile (tileClicked);
+    }
+    else if (minecraft.tool === "shovel" && (tileClicked.hasClass("grass")) || (tileClicked.hasClass("dirt"))) {
+        minecraft.moveTile (tileClicked);
 
     }
-    else if (minecraft.tool === "shovel" && (minecraft.tile === "box grass") || (minecraft.tile === "box dirt")) {
-        minecraft.storedTile = minecraft.tile;
-        $('.storedTile').removeClass().addClass(minecraft.tile).removeClass('box').addClass('storedTile');
-        tileClicked.className="box";
-        minecraft.matrix[$(this).data("i")][($(this)).data("j")] = 'box';
+    else if (minecraft.tool === "pick" && tileClicked.hasClass("rock")) {
+        minecraft.moveTile (tileClicked);
+    }
+    // to reposition the tiles that were saved you need to select the 'treasure tool' and then you can click anywhere in the 'sky' and put it there
+    else if (minecraft.tool === "treasure" && tileClicked.hasClass("sky")) {
+        var saved = $('#treasure').attr("data-saved-tile");
+        tileClicked.addClass(saved).removeClass('sky');
+        minecraft.matrix[tileClicked.data("i")][tileClicked.data("j")] = $('#treasure').attr("data-saved-tile");
         minecraft.renderBoard();
+        $('#treasure').attr("data-saved-tile", " ");
+
 
     }
-    else if (minecraft.tool === "pick" && minecraft.tile === "box rock") {
-        minecraft.storedTile = minecraft.tile;
-        $('.storedTile').removeClass().addClass(minecraft.tile).removeClass('box').addClass('storedTile');
-        tileClicked.className="box";
-        minecraft.matrix[$(this).data("i")][($(this)).data("j")] = 'box';
-        minecraft.renderBoard();
-    }
-
+    // if the selected tool does not match the tile you can an'error
     else {
-        console.log(document.getElementById(minecraft.tool));
-        var div=document.getElementById(minecraft.tool);
+
+        var div = document.getElementById(minecraft.tool);
         div.className += ' errorClick';
-        setTimeout(function(){
-            div.className='tool';
+        setTimeout(function () {
+            div.className = 'tool';
         }, 2000);
-
-
     }
+
 };
 
 
+//inner function that if the tool selects the right tile takes the class of the tile selected and saves it inside the storedTile and then reloads the matrix
+minecraft.moveTile =  function(tileClicked) {
+    minecraft.storedTile = minecraft.tile;
+    $('#treasure').attr("data-saved-tile", minecraft.tile);
+    minecraft.matrix[tileClicked.data("i")][tileClicked.data("j")] = 'box sky';
+    minecraft.renderBoard();
+};
+
+//updating the board according to the matrix
 minecraft.renderBoard = function () {
     minecraft.tiles
-        .removeClass("sky")
+
         .removeClass("tree")
         .removeClass("leaf")
         .removeClass("dirt")
@@ -183,7 +235,7 @@ minecraft.renderBoard = function () {
             minecraft.tiles.eq(i * 20 + j).addClass(minecraft.matrix[i][j]);
         }
     }
-}
+};
 minecraft.init();
 
 
